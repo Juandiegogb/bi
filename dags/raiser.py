fda06771955127311d60e6cb3718d593cdf28999
Juan Diego <juandiegar@gmail.com>
2025-01-09 11:23:26 -0500

    refactoring for run on ubuntu server

--- dags/raiser.py
+++ dags/raiser.py
@@ -1,22 +1,30 @@
 from airflow import DAG
-from airflow.operators.python import task
+from airflow.providers.postgres.hooks.postgres import PostgresHook
+from airflow.decorators import task
 from datetime import datetime
 
 
+# Definir la función para extraer datos con el decorador
+@task
+def extract_data_from_postgres():
+    # Conexión a PostgreSQL usando el nombre de la conexión definida en Airflow
+    postgres_hook = PostgresHook(postgres_conn_id="tu_conexion_postgres")
+
+    # Ejecutar una consulta SQL
+    sql = "SELECT * FROM nombre_de_tu_tabla LIMIT 10;"
+
+    # Extraer los datos
+    result = postgres_hook.get_records(sql)
+
+    # Imprimir los resultados
+    for row in result:
+        print(row)
+
+
+# Definir el DAG
 with DAG(
-    "raiser",
-    description="is not necesary",
-    catchup=False,
-    schedule="*/2 * * * *",
-    start_date=datetime(2025, 1, 1),
-    default_args={
-        "email": ["juandiegar@gmail.com"],
-        "email_on_failure": True,
-    },
+    "extract_data_postgres", start_date=datetime(2025, 1, 8), schedule_interval=None
 ) as dag:
 
-    @task(task_id="raiser_task")
-    def raiser():
-        raise ZeroDivisionError
-
-    raiser()
+    # Llamar a la función decorada para extraer los datos
+    extract_data_from_postgres()
