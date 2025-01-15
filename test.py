a59ed5fedd6b74f78b2e2b59e4f3ba0d15e0d02b
Juan Diego <juandiegar@gmail.com>
2025-01-15 10:31:23 -0500

    fixing test.py

--- test.py
+++ test.py
@@ -1,7 +1,31 @@
-from pyspark.sql import SparkSession , DataFrame
+"""
+Esta es una ETL con Python y Pyspark
+"""
 
-spark : SparkSession = SparkSession.builder.appName("test").getOrCreate()
+from time import time
+from pyspark.sql.types import StructType, StructField, DoubleType
+from pyspark.sql import SparkSession
+from config import  pg
 
-df: DataFrame = spark.createDataFrame([1000],("time"))
-print(df)
-df.show()
+started_at = time()
+spark: SparkSession = (
+    SparkSession.builder.appName("ETL")
+    .config("spark.executor.cores", "4")
+    .config("spark.executor.memory", "4g")
+    .getOrCreate()
+)
+
+pg_url = f"jdbc:postgresql://{pg['host']}:{pg['port']}/{pg['db']}"
+pg_properties: dict = {
+    "user": pg["user"],
+    "password": pg["pwd"],
+    "driver": "org.postgresql.Driver",
+}
+ended_at = time()
+total_time = round((ended_at - started_at) / 60, 2)
+
+schema = StructType([StructField("time", DoubleType(), True)])
+time_df = spark.createDataFrame([(total_time,)], schema)
+
+time_df.write.mode("append").jdbc(pg_url, "time_stats", properties=pg_properties)
+print("Stats table loaded to stage")
